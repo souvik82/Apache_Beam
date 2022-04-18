6bbf33eb387532665a2cfbc8d36555ce3063361d
Souvik Das <amisouvikdas@gmail.com>
2022-04-18 17:23:03 +0530

    Update beamDemo.py

--- beamDemo.py
+++ beamDemo.py
@@ -3,7 +3,7 @@ import apache_beam as beam
 if __name__ == '__main__':
     with beam.Pipeline() as p:
         test = (p
-                | 'Read Data' >> beam.io.ReadFromText(r"C:\Users\INE12377731\Data\actual.csv", skip_header_lines=1)
+                | 'Read Data' >> beam.io.ReadFromText(r"C:\Users\xxx\Data\actual.csv", skip_header_lines=1)
                 | 'Split Data' >> beam.Map(lambda line: line.split(','))
                 | 'Filter Data' >> beam.Filter(lambda records: records[6] == '1065')
                 | 'Join Data' >> beam.Map(lambda records: ",". join(records))
