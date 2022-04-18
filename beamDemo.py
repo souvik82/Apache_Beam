79be3971f830a69203d82f725e1cda419bd521d9
Souvik Das <amisouvikdas@gmail.com>
2022-04-18 17:23:24 +0530

    Update beamDemo.py

--- beamDemo.py
+++ beamDemo.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
                 | 'Split Data' >> beam.Map(lambda line: line.split(','))
                 | 'Filter Data' >> beam.Filter(lambda records: records[6] == '1065')
                 | 'Join Data' >> beam.Map(lambda records: ",". join(records))
-                | 'Write to Local File' >> beam.io.WriteToText(r"C:\Users\INE12377731\Data\actual", ".csv")
+                | 'Write to Local File' >> beam.io.WriteToText(r"C:\Users\xxx\Data\actual", ".csv")
                 )
